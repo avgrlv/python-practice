75de543392386875de601667c29308428142f7fe
artg2019 <artem0gorlov@gmail.com>
2024-09-11 17:54:52 +0300

    commit with any tasks

--- first/runner.py
+++ first/runner.py
@@ -1,3 +1,5 @@
+import math
+
 from first.calculator import CalculatorTask
 
 
@@ -7,3 +9,44 @@ def task_2():
         int(input('Введите второе число: ')), \
         input(task.print_operations())
     task.calculate(a, b, op)
+
+
+def task_3():
+    x, y, z = int(input('Введите x: ')), \
+        int(input('Введите y: ')), \
+        int(input('Введите z: '))
+    f = round(
+        pow((x ** 5 + 7) / (math.fabs(-6) * y), 1 / 3) / (7 - z * math.fabs(y)),
+        3)
+    print(f"Результат: {f}")
+
+
+def task_4():
+    r_1, r_2 = float(input("Введите сопротивление резистора R1: ")), \
+        float(input("Введите сопротивление резистора R2: "))
+    print(f"Общее сопротивление цепи ={round(r_1 + r_2, 1)}")
+
+
+def task_5():
+    pass
+
+
+def task_6():
+    pass
+
+
+def task_7():
+    pass
+
+
+def task_8():
+    pass
+
+
+def task_9():
+    a = int(input('Введите первое число: '))
+    b = int(input('Введите второе число: '))
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    print(f"Первое число: {a}, второе число: {b}")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from first.runner import task_2
+from first.runner import task_2, task_3, task_4, task_5, task_9
 
 if __name__ == '__main__':
-    task_2()
-
+    # task_2()
+    # task_3()
+    # task_4()
+    task_9()
